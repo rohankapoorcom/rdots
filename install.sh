e207e628b0d5eb691b561052379f4f7f7bb5e687
Rohan Kapoor <rohan@rohankapoor.com>
2014-04-19 19:11:15 -0500

    Updating script to symlink correctly

--- install.sh
+++ install.sh
@@ -13,6 +13,9 @@ fi
 # Get the current directory of the install script
 SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# Get the directory of the user
+USER_HOME="$(getent passwd $SUDO_USER | cut -d: -f6)"
+
 echo "Installing Packages"
 apt-get update
 apt-get install python-software-properties python g++ make gcc mongodb git vim -y
@@ -29,23 +32,25 @@ if xset q &>/dev/null; then
 	apt-get install sublime-text
 fi	
 
-echo "Linking bash_aliases from $SCRIPT_DIRECTORY to $HOME"
-sudo -u $USERNAME ln -s $SCRIPT_DIRECTORY/bash_aliases $HOME/.bash_aliases
+echo "Linking bash_aliases from $SCRIPT_DIRECTORY to $USER_HOME"
+sudo -u $USERNAME ln -s $SCRIPT_DIRECTORY/bash_aliases $USER_HOME/.bash_aliases
 
-echo "Linking gitconfig from $SCRIPT_DIRECTORY to $HOME"
-sudo -u $USERNAME ln -s $SCRIPT_DIRECTORY/gitconfig $HOME/.gitconfig
+echo "Linking gitconfig from $SCRIPT_DIRECTORY to $USER_HOME"
+sudo -u $USERNAME ln -s $SCRIPT_DIRECTORY/gitconfig $USER_HOME/.gitconfig
 
-echo "Linking tmux.conf from $SCRIPT_DIRECTORY to $HOME"
-sudo -u $USERNAME ln -s $SCRIPT_DIRECTORY/tmux.conf $HOME/.tmux.conf
+echo "Linking tmux.conf from $SCRIPT_DIRECTORY to $USER_HOME"
+sudo -u $USERNAME ln -s $SCRIPT_DIRECTORY/tmux.conf $USER_HOME/.tmux.conf
 
-echo "Linking vimrc from $SCRIPT_DIRECTORY to $HOME"
-sudo -u $USERNAME ln -s $SCRIPT_DIRECTORY/vimrc $HOME/.vimrc
+echo "Linking vimrc from $SCRIPT_DIRECTORY to $USER_HOME"
+sudo -u $USERNAME ln -s $SCRIPT_DIRECTORY/vimrc $USER_HOME/.vimrc
 
-echo "Linking vim from $SCRIPT_DIRECTORY to $HOME"
-sudo -u $USERNAME ln -s $SCRIPT_DIRECTORY/vim/ $HOME/.vim
+echo "Linking vim from $SCRIPT_DIRECTORY to $USER_HOME"
+sudo -u $USERNAME ln -s $SCRIPT_DIRECTORY/vim/ $USER_HOME/.vim
 
 echo "Removing sublime text 2 default config"
-sudo -u $USERNAME rm -r "$HOME/.config/sublime-text-2/Packages/User"
+sudo -u $USERNAME rm -r "$USER_HOME/.config/sublime-text-2/Packages/User"
 
-echo "Linking Sublime Text from $SCRIPT_DIRECTORY to $HOME"
-sudo -u $USERNAME ln -s $SCRIPT_DIRECTORY/sublime/User "$HOME/.config/sublime-text-2/Packages/User"
+mkdir -p "$USER_HOME/.config/sublime-text-2/Packages/"
+
+echo "Linking Sublime Text from $SCRIPT_DIRECTORY to $USER_HOME"
+sudo -u $USERNAME ln -s $SCRIPT_DIRECTORY/sublime/User "$USER_HOME/.config/sublime-text-2/Packages/User"
